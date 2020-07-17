f28faf0be2f0b42c0a8f335db648edb43d7d2004
Alex <gurievalex78@gmail.com>
2020-07-17 13:54:26 +0200

    adjust

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-document.getElementById('genera').addEventListener("click", function(){
+  var genera=document.getElementById('genera');
+  genera.addEventListener("click", function(){
   var utente = document.getElementById('utente');
   utente.classList.remove('hidden');
   utente.classList.add('show');
@@ -6,31 +7,30 @@ document.getElementById('genera').addEventListener("click", function(){
   var valoreNome = document.getElementById('nome__cognome').value;
   document.getElementById('nome__cognomeIns').innerHTML = valoreNome;
 
-  var valoreCarozza = Math.floor((Math.random() * 10) + 1);
+  var valoreCarozza = getRndInteger(1,10)
   document.getElementById('valCarozza').innerHTML = valoreCarozza;
 
-  var codiceTreno = Math.floor(Math.random() * (100000 - 90000) ) + 90000;
+  var codiceTreno = getRndInteger(9000,10000)
   document.getElementById('codTreno').innerHTML = codiceTreno;
 
   var kilometri = document.getElementById('km').value;
   var eta = document.getElementById('eta').value;
   var prezzoKm = 0.21;
   var prezzo = kilometri * prezzoKm;
+  var messaggio = 'Tariffa Standard';
   if(eta==1){
-    prezzo = prezzo - (prezzo*0.2);
-    document.getElementById('minor').innerHTML =  'Minorenne';
-  } else if (eta==2) {
-    prezzo = kilometri * prezzoKm;
-    document.getElementById('minor').innerHTML =  'Maggiorenne';
-  } else{
-      prezzo = prezzo - (prezzo*0.4);
-    document.getElementById('minor').innerHTML =  'Over 65';
+    prezzo -= prezzo*0.2;
+    messaggio =  'Minorenne';
+  } else if (eta==3) {
+    prezzo -=  prezzo*0.4;
+    messaggio= 'Tariffa Silver';
   }
-
+  document.getElementById('minor').innerHTML = messaggio;
   document.getElementById('price').innerHTML = prezzo.toPrecision(3) +' '+ '€';
 });
 
-document.getElementById('anulla').addEventListener("click", function(){
+var anulla = document.getElementById('anulla')
+anulla.addEventListener("click", function(){
   var utente = document.getElementById('utente');
     utente.classList.add('hidden');
     utente.classList.remove('show');
@@ -38,3 +38,7 @@ document.getElementById('anulla').addEventListener("click", function(){
     document.getElementById('km').value='';
     document.getElementById('eta').value=1;
 });
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
